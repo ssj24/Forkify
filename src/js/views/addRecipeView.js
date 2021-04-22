1cc538b6e87953a43f29de9fdcbbf40be6ee3705
ssj24 <sujee0824@gmail.com>
2021-04-22 23:11:16 +0900

    Add more ing feature

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -8,11 +8,15 @@ class AddRecipeView extends View{
   _overlay = document.querySelector('.overlay');
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
   _btnClose = document.querySelector('.btn--close-modal');
+  _btnUpload = document.querySelector('upload__btn');
+  _ingColumn = document.getElementById('ingColumn');
+  _plusBtn = document.querySelector('.ing__plus');
 
   constructor() {
     super();
     this._addHandlerShowWindow();
     this._addHandlerHideWindow();
+    this._addHandlerAddIng();
   }
 
   toggleWindow() {
@@ -27,6 +31,29 @@ class AddRecipeView extends View{
   _addHandlerHideWindow() {
     this._btnClose.addEventListener('click', this.toggleWindow.bind(this))
     this._overlay.addEventListener('click', this.toggleWindow.bind(this))
+  }
+
+  _addHandlerAddIng() {
+    this._plusBtn.addEventListener('click', () => {this._addIngredient(this)});
+  }
+  _addIngredient() {
+    let ings = this._ingColumn.querySelectorAll('label')
+    this._plusBtn.classList.remove('ing__plus');
+    this._plusBtn.remove();
+    const markup = `
+      \b
+      <label>Ingredient ${ings.length + 1}</label>
+      <input
+        type="text"
+        required
+        name = "Ingredient-${ings.length + 1}"
+        placeholder="Format: Quantity, Unit, Description"
+      />
+      <input type="button" class="btn ing__plus" value="+"></input>
+    `
+    this._ingColumn.insertAdjacentHTML('beforeend',markup);
+    this._plusBtn = document.querySelector('.ing__plus');
+    this._addHandlerAddIng();
   }
 
   addHandlerUpload(handler) {
